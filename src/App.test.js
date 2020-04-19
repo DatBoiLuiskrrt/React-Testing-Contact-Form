28a29f8799e5dba72f79970f4e33bc5c59de81ea
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-04-19 14:49:58 -0500

    added a test that rendered the contactForm component to test if the input values were on the document, and while also running tests I noticed that the first name input had a max length of 3 characters.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
+import ContactForm from "../src/components/ContactForm";
 
 test("renders App without crashing", () => {
-  render(<App />);
+  const {queryByText} = render(< ContactForm />);
+  queryByText(/firstName/i);
+  queryByText(/lastName/i);
+  queryByText(/email/i);
 });
